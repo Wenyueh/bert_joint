0464e34abfab40b222be69493140a2b11e8c5a03
Wenyueh <norahua1996@outlook.com>
2021-03-24 00:32:36 -0400

    bug fixed

--- my_prepare_nq_data.py
+++ my_prepare_nq_data.py
@@ -20,18 +20,20 @@ import argparse
 # add candidates to example
 
 
-def get_answer(args):
+def get_examples(args):
     with gzip.GzipFile(args.train_data, "rb") as f:
         for line in f:
             data = json.loads(line)
-            create_example(data)
+            create_example(args, data)
 
 
 def create_example(args, data):
-    # still need to filter candidates by top_level, etc
+    # first, filter the candidates
+    filtered_candidates_list = filtered_candidates(args, data)
+    # add type for each candidate
     # tested correct
     counts = collections.defaultdict(int)
-    for candidate in data["long_answer_candidates"]:
+    for candidate in filtered_candidates_list:
         # add type_and_position to each candidate
         first_token_position = candidate["start_token"]
         first_token = data["document_tokens"][first_token_position]["token"]
@@ -60,6 +62,7 @@ def create_example(args, data):
     # (d) span end within the long answer string
     # (e) type
     answer = {
+        # index in the original long_answer_candidates instead of the filtered version
         "annotation_index": candidate_idx,
         "span_text": "",
         "span_start": -1,
@@ -68,94 +71,122 @@ def create_example(args, data):
     }
 
     # define answer type
-    if annotation["yes_no_answer"] == "YES":
-        answer["type"] == "yes"
-    elif annotation["yes_no_answer"] == "NO":
-        answer["type"] == "no"
+    if annotation:
+        if annotation["yes_no_answer"] == "YES":
+            answer["type"] == "yes"
+        elif annotation["yes_no_answer"] == "NO":
+            answer["type"] == "no"
 
-    # tested correct
-    def get_candidate_text(data, candidate_idx):
-        TextSpan = collections.namedtuple("TextSpan", "text_positions text")
+        if annotation["long_answer"]["candidate_index"] >= 0:
+            answer["span_text"] = get_candidate_text(
+                data, data["long_answer_candidates"], candidate_idx
+            ).text
+            answer["span_start"] = 0
+            answer["span_end"] = len(answer["span_text"])
 
-        if candidate_idx < 0 or candidate_idx >= len(data["long_answer_candidates"]):
-            return TextSpan([], "")
+        # if short
+        # tested correct
+        if not short_span_positions == (-1, -1):
+            start_position = short_span_positions[0]
+            end_position = short_span_positions[1]
+            answer["type"] = "short"
+            candidate_text = get_candidate_text(
+                data, data["long_answer_candidates"], candidate_idx
+            ).text
+            answer["span_text"] = candidate_text[start_position:end_position]
+            answer["span_start"] = start_position
+            answer["span_end"] = end_position
 
-        start_position = data["long_answer_candidates"][candidate_idx]["start_token"]
-        end_position = data["long_answer_candidates"][candidate_idx]["end_token"]
-        token_positions = []
-        span = []
-        for i in range(start_position, end_position + 1):
-            token = data["document_tokens"][i]
-            if token["html_token"] == False:
-                word = token["token"].replace(" ", "")
-                token_positions.append(i)
-                span.append(word)
+        # find new candidate idx in filtered candidate list
+        gold_start_token = annotation["long_answer"]["start_token"]
+        for idx, cand in enumerate(filtered_candidates_list):
+            if cand["start_token"] == gold_start_token:
+                candidate_idx = idx
 
-        span = " ".join(span)
+        # aggregate candidate list
+        # add an empty candidate at the beginning
+        # tested correct
+        offset = 0
+        candidates_text = ["[ContextId=%d] %s" % (-1, "[NoLongAnswer]")]
+        offset += len(candidates_text[-1]) + 1
+        candidates_map = [-1, -1]
+        candidate_indices = [-1]
+        for idx, cand in enumerate(filtered_candidates_list):
+            if idx < args.max_candidates:
+                candidate_indices.append(idx)
+                # insert the number of candidates
+                # the type of candidates
+                # and the number this type occurs so far
+                candidates_text.append(
+                    "[ContextId=%d] %s" % (idx, cand["type_and_position"])
+                )
+                # +1 because of ' '
+                offset += len(candidates_text[-1]) + 1
+                if idx == candidate_idx:
+                    answer["span_start"] += offset
+                    answer["span_end"] += offset
 
-        return TextSpan(token_positions, span)
+                candidates_text.append(
+                    get_candidate_text(data, filtered_candidates_list, idx).text
+                )
+                offset += len(candidates_text[-1]) + 1
+                # use [-1, -1] to correspond to [contextid=] and [type_and_position]
+                candidates_map.extend([-1, -1])
+                candidates_map.extend(
+                    get_candidate_text(
+                        data, filtered_candidates_list, idx
+                    ).text_positions
+                )
+            else:
+                break
 
-    # if short
-    # tested correct
-    if not short_span_positions == (-1, -1):
-        start_position = short_span_positions[0]
-        end_position = short_span_positions[1]
-        answer["type"] = "short"
-        candidate_text = get_candidate_text(data, candidate_idx).text
-        answer["span_text"] = candidate_text[start_position:end_position]
-        answer["span_start"] = start_position
-        answer["span_end"] = end_position
+        candidates_text = " ".join(candidates_text)
 
-    # if long
-    if annotation["long_answer"]["candidate_index"] >= 0:
-        answer["span_text"] = get_candidate_text(data, candidate_idx).text
-        answer["span_start"] = 0
-        answer["span_end"] = len(answer["span_text"])
+        example = {
+            "name": data["document_title"],
+            "id": data["example_id"],
+            "question": [data["question_text"]],
+            "answer": [answer],
+            "has_correct_candidate": candidate_idx in candidate_indices,
+            "candidates": candidates_text,
+            "candidates_map": candidates_map,
+        }
 
-    # aggregate candidate list
-    # add an empty candidate at the beginning
-    # tested correct
-    offset = 0
-    candidates_text = ["[ContextId=%d] %s" % (-1, "[NoLongAnswer]")]
-    offset += len(candidates_text[-1]) + 1
-    candidates_map = [-1, -1]
-    for idx, cand in enumerate(data["long_answer_candidates"]):
-        if idx < args.max_candidates:
-            # insert the number of candidates
-            # the type of candidates
-            # and the number this type occurs so far
-            candidates_text.append(
-                "[ContextId=%d] %s" % (idx, cand["type_and_position"])
-            )
-            # +1 because of ' '
-            offset += len(candidates_text[-1]) + 1
-            if idx == candidate_idx:
-                answer["span_start"] += offset
-                answer["span_end"] += offset
-
-            candidates_text.append(get_candidate_text(data, idx).text)
-            offset += len(candidates_text[-1]) + 1
-            # use [-1, -1] to correspond to [contextid=] and [type_and_position]
-            candidates_map.extend([-1, -1])
-            candidates_map.extend(get_candidate_text(data, idx).text_positions)
-        else:
-            break
-
-    candidates_text = " ".join(candidates_text)
-
-    candidate_indices = [-1] + list(range(args.max_candidates))
-
-    example = {
-        "name": data["document_title"],
-        "id": data["example_id"],
-        "question": [data["question_text"]],
-        "answer": [answer],
-        "has_correct_candidate": candidate_idx in candidate_indices,
-        "candidates": candidates_text,
-        "candidates_map": candidates_map,
-    }
+    else:
+        example = {
+            "name": data["document_title"],
+            "id": data["example_id"],
+            "question": [data["question_text"]],
+            "answer": [answer],
+            "has_correct_candidate": False,
+            "candidates": "",
+            "candidates_map": [],
+        }
 
     return example
+
+
+# tested correct
+def get_candidate_text(data, candidates_list, candidate_idx):
+    TextSpan = collections.namedtuple("TextSpan", "text_positions text")
+
+    if candidate_idx < 0 or candidate_idx >= len(candidates_list):
+        return TextSpan([], "")
+
+    start_position = candidates_list[candidate_idx]["start_token"]
+    end_position = candidates_list[candidate_idx]["end_token"]
+    token_positions = []
+    span = []
+    for i in range(start_position, end_position + 1):
+        token = data["document_tokens"][i]
+        if token["html_token"] == False:
+            word = token["token"].replace(" ", "")
+            token_positions.append(i)
+            span.append(word)
+
+    span = " ".join(span)
+
+    return TextSpan(token_positions, span)
 
 
 # tested correct
@@ -166,21 +197,24 @@ def get_first_annotation(data):
         key=lambda a: a["long_answer"]["candidate_index"],
     )
 
-    annotation = sorted_annotations[0]
+    if sorted_annotations:
+        annotation = sorted_annotations[0]
 
-    cand_index = annotation["long_answer"]["candidate_index"]
+        cand_index = annotation["long_answer"]["candidate_index"]
 
-    if annotation["short_answers"]:
-        short_start_token = annotation["short_answers"][0]["start_token"]
-        short_end_token = annotation["short_answers"][-1]["end_token"]
-        long_start_token = annotation["long_answer"]["start_token"]
-        start_offset = compute_offset(data, long_start_token, short_start_token)
-        end_offset = compute_offset(data, long_start_token, short_end_token)
+        if annotation["short_answers"]:
+            short_start_token = annotation["short_answers"][0]["start_token"]
+            short_end_token = annotation["short_answers"][-1]["end_token"]
+            long_start_token = annotation["long_answer"]["start_token"]
+            start_offset = compute_offset(data, long_start_token, short_start_token)
+            end_offset = compute_offset(data, long_start_token, short_end_token)
 
-        return annotation, cand_index, (start_offset, end_offset)
+            return annotation, cand_index, (start_offset, end_offset)
 
+        else:
+            return annotation, cand_index, (-1, -1)
     else:
-        return annotation, cand_index, (-1, -1)
+        return None, -1, (-1, -1)
 
 
 # tested correct
@@ -207,13 +241,40 @@ def compute_offset(data, token_idx_1, token_idx_2):
     return char_offset
 
 
+def filtered_candidates(args, data):
+    filtered_candidates = []
+    for idx, cand in enumerate(data["long_answer_candidates"]):
+        if get_candidate_text(
+            data, data["long_answer_candidates"], idx
+        ).text.strip() == "" or (
+            args.skip_non_top_level_candidates and not cand["top_level"]
+        ):
+            continue
+        else:
+            filtered_candidates.append(cand)
+
+    return filtered_candidates
+
+
+# process, such as tokenizing, the examples
+class process_examples:
+    def __init__(self, args, tokenizer):
+        self.training = args.train
+        self.tokenizer = tokenizer
+
+    def process(example):
+        pass
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--train_data", type=str, default="data/dev/train/nq-train.jsonl.gz"
     )
     parser.add_argument("--max_candidates", type=int, default=50)
+    parser.add_argument("--train", type=bool, default=True)
+    parser.add_argument("--skip_non_top_level_candidates", type=bool, default=True)
 
     args = parser.parse_args()
 
-    get_answer(args)
+    get_examples(args)
